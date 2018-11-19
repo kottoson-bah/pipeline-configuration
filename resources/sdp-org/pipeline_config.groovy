e597bcf3899a7c3b046a912fdc5cc4b96fbab2c3
kottoson-bah <45176300+kottoson-bah@users.noreply.github.com>
2018-11-19 11:49:43 -0500

    Update pipeline_config.groovy

--- resources/sdp-org/pipeline_config.groovy
+++ resources/sdp-org/pipeline_config.groovy
@@ -1,1 +1,10 @@
+sdp_image_repository = "https://docker-registry.default.svc:5000"
+sdp_image_repository_credential = "sdp-jenkins-docker-registry"
 
+application_image_repository = "docker-registry.default.svc:5000/kottoson-images"
+application_image_repository_credential = "sdp-jenkins-docker-registry"
+
+libaries{
+  github_enterprise
+  docker
+}
