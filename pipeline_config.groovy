e70e020a9851b4f436b195441f658f11eebae904
kottoson <kjo9fq@virginia.edu>
2019-01-30 11:43:52 -0500

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,6 +4,13 @@ libraries{
     registry = "docker-registry.default.svc:5000/keegan-sdp"
     cred = "openshift-docker-registry"
   }
+  sonarqube
+  sdp{
+    images{
+      registry = "https://docker-registry.default.svc:5000
+      repo = "keegan-sdp"
+      cred = "openshift-docker-registry"
+  }
 }
 
 keywords{
