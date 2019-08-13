5ec031622313cad1185c0da8d08d73ca13bd3405
kottoson-bah <45176300+kottoson-bah@users.noreply.github.com>
2019-08-13 13:08:17 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,40 +2,4 @@ libraries{
   github{
     source_type = 'github'
   }
-  docker{
-    registry = "docker-registry.default.svc:5000"
-    cred = "openshift-docker-registry"
-    repo_path_prefix = "keegan-test-images"
-  }
-  sdp{
-    images{
-      registry = "http://docker-registry.default.svc:5000"
-      repository = "keegan-sdp"
-      cred = "openshift-docker-registry"
-    }
-  }
-  openshift{
-    url = "https://master.ocp-dev.microcaas.net:8443"
-    helm_configuration_repository = "https://github.com/kottoson-bah/sdp-example-helm-config.git"
-    helm_configuration_repository_credential = "github"
-    tiller_namespace = "keegan-test-tiller"
-    tiller_credential = "keegan-test-tiller"
-    //promote_previous_image = false
-  }
-  owasp_zap{
-    target = "http://sdp-example-proj-dev.apps.ocp-dev.microcaas.net"
-  }
-}
-
-application_environments {
-  dev {
-    short_name = "dev"
-    long_name = "Development"
-  }
-}
-
-keywords{
- merge = true 
- develop = /^[dD]evelop$/
- master = /^[mM]aster$/
 }
